02823490b3494c4d2e29d20f9ebfe26853313847
Alexander Wong <alwong34@uw.edu>
2017-12-04 15:04:10 -0800

    made tabs

--- BB-group2-Final-Project/ui.R
+++ BB-group2-Final-Project/ui.R
@@ -10,24 +10,11 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
+shinyUI(navbarPage("Sleeping Alone",
+                   tabPanel("Intro"),
+                   tabPanel("Background"),
+                   tabPanel("Graph"),
+                   tabPanel("Graph"),
+                   tabPanel("Graph"),
+                   tabPanel("Conclusion")
 ))
