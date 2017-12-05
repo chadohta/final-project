4cd7932815ec43bbdf1d7f7682f8a05415c5dc5c
Jessica Lou <jlou.xu@gmail.com>
2017-12-04 16:33:20 -0800

    removed comments that were auto added

--- BB-group2-Final-Project/ui.R
+++ BB-group2-Final-Project/ui.R
@@ -1,21 +1,12 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(navbarPage("Sleeping Alone",
                    tabPanel("Intro"),
-                   tabPanel("Background", 
-                            titlePanel("Background Information")),
+                   tabPanel("Background"),
+                   tabPanel("Graph"),
                    tabPanel("Graph"),
                    fluidPage(
+                     p("These ")
                      # helps us stay together graph
                      # get better sleep graph
                      # improved sex life graph
