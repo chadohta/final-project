5099fd8831b2e97e17a58f181b287d81ec38154b
Jessica Lou <jlou.xu@gmail.com>
2017-12-04 17:07:53 -0800

    updated graph2

--- BB-group2-Final-Project/ui.R
+++ BB-group2-Final-Project/ui.R
@@ -5,7 +5,11 @@ shinyUI(navbarPage("Sleeping Alone",
                    tabPanel("Background"),
                    tabPanel("Graph"),
                    tabPanel("Graph", fluidPage( #mine
-                     p("These ")
+                     h5("Participants were asked to choose from a range of \"Strongly Agree\" to \"Strongly Disagree\" to the 
+                       following statements:"), 
+                     p("- \"Sleeping in separate beds helps us to stay together.\""), 
+                     p("- \"We sleep better when we sleep in separate beds.\""), 
+                     p("- \"Our sex life has improved as a result of sleeping in separate beds.\"")
                      # helps us stay together graph
                      # get better sleep graph
                      # improved sex life graph
