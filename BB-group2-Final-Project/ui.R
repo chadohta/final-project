40962103778c18d24639e5201b232166967ce019
Jessica Lou <jlou.xu@gmail.com>
2017-12-04 16:39:39 -0800

    more fixing

--- BB-group2-Final-Project/ui.R
+++ BB-group2-Final-Project/ui.R
@@ -1,28 +1,19 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 6ca1b3d3d911fb4499f96628c4add6ab5cbad6c0
 library(shiny)
 
 shinyUI(navbarPage("Sleeping Alone",
                    tabPanel("Intro"),
                    tabPanel("Background"),
                    tabPanel("Graph"),
-                   tabPanel("Graph"),
-<<<<<<< HEAD
-                   fluidPage(
+                   tabPanel("Graph", fluidPage( #mine
                      p("These ")
                      # helps us stay together graph
                      # get better sleep graph
                      # improved sex life graph
-                   ),
+                     )),
                    tabPanel("Graph"),
-=======
                    tabPanel("Graph", fluidPage(
-                                      titlePanel("Testing"))),
->>>>>>> 6ca1b3d3d911fb4499f96628c4add6ab5cbad6c0
-                   tabPanel("Conclusion"),
-                   fluidPage(
-                     p("insert paragraph here")
-                   )
+                     titlePanel("Testing"))),
+                   tabPanel("Conclusion", fluidPage( #mine
+                     #conclusion statements here
+                   ))
 ))
